27ad188ebeebdeaf9e95c0e07474515a7c94cabc
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2021-12-22 16:50:18 +0000

    Functionality working

--- main.js
+++ main.js
@@ -4,13 +4,39 @@
 
 // Withdraw
 
-// You are a cash machine. Given an inputed abount less than or equal to £300, dispense from you cash machine that number in £20, £10 or £5 notes. If user enters an amount greater than £300, return a message EXCACTLY saying "You cannot do that to me. Do you think I am made of money?".
-
-
+// You are a cash machine. Given an inputed abount less than or equal to £100, dispense from you cash machine that number in £20, £10 or £5 notes.
+//  If user enters an amount greater than £300, return a message EXCACTLY saying "You cannot do that to me. Do you think I am made of money?".
 // max - can only dispense £300 a day in one go
 
-
 //£20 £10 £5
-// calculate the minimal amount of notes the cash machine can dispense 
+// calculate the minimal amount of notes the cash machine can dispense
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function calculateNotes(money) {
+  const array = [];
+
+  if (money % 20 === 0) {
+    array.push(money / 20);
+    array[1] = 0;
+    array[2] = 0;
+  } else if (money % 20 !== 0 && money % 10 === 0) {
+    //Calculate how many 20s and push into the array
+    let twenties = Math.floor(money / 20);
+    array.push(twenties);
+    //Calculate the 10s
+    let tens = (money % 20) / 10;
+    array.push(tens);
+    array[2] = 0;
+  } else {
+    //Calculate how many 20s and push into the array
+    let twenties = Math.floor(money / 20);
+    array.push(twenties);
+    //Calculate the 10s
+    let tens = Math.floor((money - 20 * twenties) / 10);
+    array.push(tens);
+    array[2] = 1;
+  }
+
+  return Number(array.join(""));
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,14 @@
 //👉 Write your tests below here:
+import { calculateNotes } from "./main";
+
+const output = [
+  [100, 500],
+  [70, 310],
+  [65, 301],
+];
+
+describe("Calculate minimum amount of notes needed", function () {
+  it.each(output)(`Should return %i as %i`, function (money, notes) {
+    expect(calculateNotes(money)).toBe(notes);
+  });
+});
