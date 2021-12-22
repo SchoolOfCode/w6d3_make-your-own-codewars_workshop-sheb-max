4509a5e976e9e2dce594d9e9aba837551dade9d0
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2021-12-22 15:42:24 +0000

    Problem statement created

--- main.js
+++ main.js
@@ -2,4 +2,15 @@
 👉 Write your kata here!
 */
 
+// Withdraw
+
+// You are a cash machine. Given an inputed abount less than or equal to £300, dispense from you cash machine that number in £20, £10 or £5 notes. If user enters an amount greater than £300, return a message EXCACTLY saying "You cannot do that to me. Do you think I am made of money?".
+
+
+// max - can only dispense £300 a day in one go
+
+
+//£20 £10 £5
+// calculate the minimal amount of notes the cash machine can dispense 
+
 //👉 Write the function your CodeWarriors will start with below here:
